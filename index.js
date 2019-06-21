42c0f79fc66c591e08b5944467f93cff9b71ba31
Tyquan <tyquanjreddick@gmail.com>
2019-06-20 21:59:07 -0400

    uncomment server section

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const express = require('express');
 const client = require('twilio')(twilioEndPoint, twilioAuthToken);
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res, next) => {
-//     const Response = new VoiceResponse();
+app.get('/', (req, res, next) => {
+    const Response = new VoiceResponse();
 
-//     Response.say("Hey Daddy This is Ty Trying out my new Phone Number App");
+    Response.say("Hey This is Ty Trying out my new Phone Number App");
 
-//     res.send(Response.toString());
+    res.send(Response.toString());
 
-//     next();
-// });
-
-// app.listen(serverPort, () => {
-//     console.log(`Server is running on port ${serverPort}`);
-// });
-
-client.calls.create({
-    url: 'https://demo.twilio.com/welcome/voice/',
-    to: receiverPhoneNumber,
-    from: trialPhoneNumber
-}).then((call) => {
-    console.log(`Call accepted! Call SID: ${call.sid}`);
-}).catch((err) => {
-    console.log(`Error making the phone call:`, err);
+    next();
 });
+
+app.listen(serverPort, () => {
+    console.log(`Server is running on port ${serverPort}`);
+});
+
+// client.calls.create({
+//     url: 'https://demo.twilio.com/welcome/voice/',
+//     to: receiverPhoneNumber,
+//     from: trialPhoneNumber
+// }).then((call) => {
+//     console.log(`Call accepted! Call SID: ${call.sid}`);
+// }).catch((err) => {
+//     console.log(`Error making the phone call:`, err);
+// });
